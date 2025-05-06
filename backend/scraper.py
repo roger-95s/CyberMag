8d8f2968427a96469de149a0d34d9488df4dfcd4
roger-95s <rogeresleitercc@gmail.com>
2025-05-06 12:57:02 -0700

    Changing tag_guide.py and Working on scraper

--- backend/scraper.py
+++ backend/scraper.py
@@ -4,10 +4,6 @@ from bs4 import BeautifulSoup
 from tag_guide import TAG_GUIDE
 
 def scrape_site(url, selectors):
-    """
-    Fetches and parses a single site using the tag selectors.
-    Returns a dict with title, content, and preview.
-    """
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -50,31 +46,4 @@ def scrape_all():
 
 
 
-
-# Fetcher function that fetch and handle content that will be use to store an Analyze 
-# def fetch_latest_articles(limit=3):
-#     """
-#     Scrapes latest cybersecurity articles from NEWS_SOURCE using BeautifulSoup.
-#     Returns a list of dictionaries: title, url, and content.
-#     """
-
-#     articles = []
-#     try:
-#         response = requests.get(URL_SOURCE)
-#         soup = BeautifulSoup(response.text, "html.parser")
-#         article_link = soup.find_all('a', class_='story-link', limit=limit)
-
-#         for link in article_link:
-#             # Identify the article Url, tittle, main content
-#             url = link.get('href')
-#             title = link.find('h2', class_='home-title').text.strip()    
-            
-#             articles.append({
-#                 'url': url,
-#                 'title': title
-#             })        
-#     except Exception as e:
-#         print(f"Error accessing NEWS_SOURCE: {e}")
-    
-#     return articles
 
