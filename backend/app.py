67c7bf39d74533205dcafdb2e790d1a63c659e8f
roger-95s <rogeresleitercc@gmail.com>
2025-06-05 15:55:39 -0700

    Last commitment

--- backend/app.py
+++ backend/app.py
@@ -1,46 +1,20 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from .models import get_all_reports, save_report
-from .scraper import fetch_latest_articles
-from .llm_tools import analyze_article
-
-# print(fetch_latest_articles())
+from .scraper import scraper
 
 # Create Flask app
 app = Flask(__name__)
 CORS(app)
 
+
 #  Api router: fetch and analyze latest cyber news
 @app.route("/api/new", methods=["GET"])
 def get_news():
-    """
-    Fetch latest cyber news and analyze using LLM.
-    """
-    print("🔍 Called /api/new route")
-
-    # Fetch latest articles 
-    articles = fetch_latest_articles()
-
-    reports = []
-
-    # process each article 
-    for article in articles:
-        try:
-            #  First analyze  the article
-            report = analyze_article(article) 
-            print(f"🤞{report}")
-            reports.append(report)
-            
-            # Call save_report for each article
-            saved_reports_db = save_report(report_data=report)
-            print(f"🎲 {saved_reports_db}")
-
-        except Exception as e:
-            print(f"❌ New_report exption error: {e}")
-
-    return reports
-
-
+    
+    return scraper()
+    
+   
 
 # API route: return all stored reports 
 @app.route("/api/reports", methods=["GET"])
