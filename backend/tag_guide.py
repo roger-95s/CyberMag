731400df7d166c8c93c1dc6c9e983c04e80f8195
roger-95s <rogeresleitercc@gmail.com>
2025-06-06 12:14:12 -0700

    Adding onother web site. https://www.darkreading.com/

--- backend/tag_guide.py
+++ backend/tag_guide.py
@@ -6,13 +6,25 @@ https://www.darkreading.com/,
 , 'ListPreview-Title'
 """
 # Global Dict/List 
-list_of_sites = [{
-        'name': 'Hacker News',
+list_of_sites = [
+    {
+        'name': 'The Hacker News',
         'url': 'https://thehackernews.com/',
         'selectors': {
                 'url_selector': {'tag': 'a', 'class': 'story-link'},
                 'title_selector': {'tag': 'h2', 'class': 'home-title'},
         }
-}]
+    },
+    {
+        'name': 'DarkReading',
+        'url': 'https://www.darkreading.com/',
+        'selectors': {
+                'url_selector': {'tag': 'a', 'class': 'ListPreview-Title'},
+                'title_selector': {'tag': 'a', 'class': 'ListPreview-Title'},
+        }
+    },
+
+    # Add more sites
+]
 
 
