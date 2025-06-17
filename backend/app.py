68667436e08ee344d51c723a4ca40cfa11e9c9a9
roger-95s <rogeresleitercc@gmail.com>
2025-06-17 13:12:52 -0700

    Changing new to index

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
-from .scraper import extract_data, articles
+from .scraper import fetch_data
 from .models import get_all_reports, save_report
 
 
@@ -10,25 +10,19 @@ CORS(app)
 
 
 # Api router: fetch and analyze latest cyber news
-@app.route("/api/rawdata", methods=["GET", "POST"])
-def rawdata():  
+@app.route("/api/datafetched", methods=["GET", "POST"])
+def index():  
     """
         Display data that was just extracted and return it. 
         Into a dict_list or a json
     """ 
-    # Pass raw data extracted from extrac_data()
-    data = []
+    datas_reports = get_all_reports()
+    # Display data on a home page
+    for data in datas_reports:
 
-    if data:
-        try:
-            data.append(articles)
-            print(f"✅ Data was passed successful: {data}")
+        return jsonify(data)
 
-            return jsonify(data['name'])
-        except Exception as e:
-            print(f"❌ Error during opperation: {e}")
-    else:
-        return jsonify(f"👨‍💻⚒️ We're Working soon you will be able to see the result")
+    return jsonify(f"👨‍💻⚒️ We're Working soon you will be able to see the result")
 
 # API route: return all stored reports 
 @app.route("/api/reports", methods=["GET"])
