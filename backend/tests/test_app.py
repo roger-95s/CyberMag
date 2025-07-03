041bd043707b023f9fd150bcbf41e921858b6a5e
roger-95s <rogeresleitercc@gmail.com>
2025-07-03 15:22:56 -0700

    Action(Ci_test): Adding more responsive integration CI locally and remote

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -2,15 +2,6 @@
 
 # backend/tests/test_app.py
 from flask.testing import FlaskClient
-import pytest
-from backend.app import app  # adjust import
-
-
-@pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
 
 
 def test_health_check(client: FlaskClient):
