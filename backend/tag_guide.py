b7b22b6b45c8f561cf0ead567757c5aeb5617a12
roger-95s <rogeresleitercc@gmail.com>
2025-07-17 18:08:21 -0700

    uptade(tag_guide):  content_selector: {                 tag: p,                 ancestor_tag: div,                 ancestor_class: c-entry-content

--- backend/tag_guide.py
+++ backend/tag_guide.py
@@ -14,6 +14,11 @@ list_of_sites = [
         "selectors": {
             "url_selector": {"tag": "a", "class": "story-link"},
             "title_selector": {"tag": "h2", "class": "home-title"},
+            "content_selector": {
+                "tag": "p",
+                "ancestor_tag": "div",
+                "ancestor_class": "body-post__content",
+            },
         },
     },
     {
@@ -22,6 +27,11 @@ list_of_sites = [
         "selectors": {
             "url_selector": {"tag": "h2", "class": "c-card__title"},
             "title_selector": {"tag": "h2", "class": "c-card__title"},
+            "content_selector": {
+                "tag": "p",
+                "ancestor_tag": "div",
+                "ancestor_class": "c-entry-content",
+            },
         },
     },
     # Add more sites
