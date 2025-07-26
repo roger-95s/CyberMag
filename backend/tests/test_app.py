e31fed8696eac236744f8aaeb0bb0efed6e1dc6c
roger-95s <rogeresleitercc@gmail.com>
2025-07-25 17:40:49 -0700

    Updates

--- backend/tests/test_app.py
+++
@@ -1,9 +0,0 @@
-"""Module providing a function printing python version."""
-
-# backend/tests/test_app.py
-from flask.testing import FlaskClient
-
-
-def test_health_check(client: FlaskClient):
-    response = client.get("/post")  # or your main route
-    assert response.status_code == 200
