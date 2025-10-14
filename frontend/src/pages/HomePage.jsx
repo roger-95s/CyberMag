a17e3daf71a8609b9115cc2ba94e5c63e1af681c
Roger E. Campo Cordova <rogeresleitercc@gmail.com>
2025-10-14 06:57:14 -0700

    Fix/id (#115)
    * fix: assign IDs to articles and unify article building logic
    
    * fix id cards
    
    ---------
    
    Co-authored-by: cesartomedes <cesartomedes@gmail.com>

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -21,6 +21,7 @@ function HomePage() {
     setData((prev) => ({ ...prev, loading: true }));
 
     fetch(`/api/home?page=${currentPage}&limit=${articlesPerPage}`)
+
       .then((res) => {
         if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
         return res.json();
@@ -88,10 +89,9 @@ function HomePage() {
                   key={index}
                   onClick={() => setCurrentPage(index + 1)}
                   className={`px-4 py-2 rounded-md border text-sm font-medium transition-colors
-                    ${
-                      currentPage === index + 1
-                        ? "bg-blue-600 text-white border-blue-600"
-                        : "bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-100 border-gray-400 hover:bg-gray-300 dark:hover:bg-gray-600"
+                    ${currentPage === index + 1
+                      ? "bg-blue-600 text-white border-blue-600"
+                      : "bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-100 border-gray-400 hover:bg-gray-300 dark:hover:bg-gray-600"
                     }`}
                 >
                   {index + 1}
