1558fa62680918a9eb21adc1ae13dec30d6363a1
Roger E. Campo Cordova <rogeresleitercc@gmail.com>
2025-10-25 15:45:38 -0700

    ai_content_analyst: This feature was commented for fixing code standards (#123)
    * ai_content_analyst: This feature was commented for fixing code standards
    
    * Uncomment code for AI content analysis functionality
    
    The max-chunks was not defined with a value and caused an error when calling it since it required the value None. Additionally, there was an if statement that checked, but below it there was a condition that would execute if not. It turns out that condition did not have an else to determine whether to do it or not.
    
    Signed-off-by: CrafteosK <105567444+CrafteosK@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: CrafteosK <105567444+CrafteosK@users.noreply.github.com>
    Co-authored-by: CrafteosK <105567444+CrafteosK@users.noreply.github.com>

--- backend/ai_content_analyst.py
+++ backend/ai_content_analyst.py
@@ -1,73 +1,78 @@
-# from ollama import generate
-# from .content import data
-# import json
-# import os
+from ollama import generate
+from .content import data
+import json
+import os
 
 
-# print("AI ==================================================!! ")
+print("AI ==================================================!! ")
 
 
-# def file_open():
-#     # Join path components
-#     prompt_path = os.path.join(
-#         os.path.dirname(__file__),
-#         "/home/hackme/portfolio/cybermag/backend/cyberattack_analysis_prompt.json",
-#     )
-#     # print(f"File path: {prompt_path}")
+def file_open():
+    """
+    Open json file function
+    """
+    prompt = []
 
-#     # Check if a file exists
-#     if os.path.exists(prompt_path):
-#         print(f"{os.path.basename(prompt_path)} exists.")
-#         with open(prompt_path, encoding="utf-8") as f:
-#             prompt_template = json.load(f)
-#             # print(f"Json info load: {prompt_template}")
+    # Join path components
+    prompt_path = os.path.join(
+        os.path.dirname(__file__),
+        "/home/hackme/portfolio/cybermag/backend/cyberattack_analysis_prompt.json",
+    )
+    # print(f"File path: {prompt_path}")
 
-#             prompt = f"{prompt_template['description']}\n\nArticle:\n{data}\n\nInstructions:\n{json.dumps(prompt_template['instructions'], indent=2, ensure_ascii=False)}"
+    # Check if a file exists
+     if os.path.exists(prompt_path):
+        print(f"{os.path.basename(prompt_path)} exists.")
+     else:
+        print(f"{os.path.basename(prompt_path)} does not exist.")
 
-#         return prompt
-#     else:
-#         print(f"{os.path.basename(prompt_path)} does not exist.")
+    with open(prompt_path, encoding="utf-8") as f:
+        prompt_template = json.load(f)
+        # print(f"Json info load: {prompt_template}")
+    prompt = f"{prompt_template['description']}\n\nArticle:\n{data}\n\nInstructions:\n{json.dumps(prompt_template['instructions'], indent=2, ensure_ascii=False)}"
 
-
-# # Function that handle ai work
-# def gemma_cyber_analyst(prompt, max_chunks=None):
-#     """
-#     Generate Cuybersecurity analysis from the Gemma3
-#     :param prompt: Full text prompt to send to the LLM.
-#     :param max_chunks: Limit the number of streamed chunks.
-#     :return: Complete response as a string.
-#     """
-#     print("==== Generating Analysis ====")
-#     response_text = ""
-
-#     try:
-#         for i, chunk in enumerate(generate("gemma3", prompt, stream=True)):
-#             response_text += chunk.get("response", "")
-#             # print(f"Response_text: {response_text}")
-#             # print()
-#             # print("AI ==================================================!! ")
-#             print(chunk.get("response", ""), end="", flush=True)
-#             # print()
-#             # print("AI ==================================================!! ")
-
-#             if max_chunks and (i + 1) >= max_chunks:
-#                 break
-
-#     except Exception as e:
-#         print(f"Error generating analysis: {e}")
-#         return None
+    return prompt
 
 
-# # save ai output on data base
-# # def
+# Function that handle ai work
+def gemma_cyber_analyst(prompt, max_chunks=None):
+    """
+    Generate Cybersecurity analysis from the Gemma3
+    :param prompt: Full text prompt to send to the LLM.
+    :param max_chunks: Limit the number of streamed chunks.
+    :return: Complete response as a string.
+    """
+    print("==== Generating Analysis ====")
+    response_text = ""
+
+    try:
+        for i, chunk in enumerate(generate("gemma3", prompt, stream=True)):
+            response_text += chunk.get("response", "")
+            # print(f"Response_text: {response_text}")
+            # print()
+            # print("AI ==================================================!! ")
+            print(chunk.get("response", ""), end="", flush=True)
+            # print()
+            # print("AI ==================================================!! ")
+
+            if max_chunks and (i + 1) >= max_chunks:
+                break
+
+    except Exception as e:
+        print(f"Error generating analysis: {e}")
+        return None
 
 
-# # Main code
-# prompt = file_open()
-# # print(f"Prompt: {prompt}")
+# save ai output on data base
+# def
 
-# # Call file_open fucn
-# file_open()
 
-# # Calling gemma3
-# gemma_cyber_analyst(prompt=prompt)
+# Main code
+prompt = file_open()
+# print(f"Prompt: {prompt}")
+
+# Call file_open fucn
+file_open()
+
+# Calling gemma3
+gemma_cyber_analyst(prompt=prompt, max_chunks=None)
