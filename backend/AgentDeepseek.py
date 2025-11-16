89e730647139cfff694b049e29440ed7f58e45a3
roger-95s <rogeresleitercc@gmail.com>
2025-11-16 11:39:34 -0800

    creatied 3rd agent using Deepseek

--- backend/AgentDeepseek.py
+++ backend/AgentDeepseek.py
@@ -1,19 +1,20 @@
+
 from ollama import generate
 
 
 # Function that handle ai work
-def llama_cyber_analyst(prompt, max_chunks=None):
+def deepseek_cyber_analyst(prompt, max_chunks=None):
     """
-    Generate Cybersecurity analysis from the Llama 3.2
+    Generate Cybersecurity analysis from the Deepseek
     :param prompt: Full text prompt to send to the LLM.
     :param max_chunks: Limit the number of streamed chunks.
     :return: Complete response as a string.
     """
-    print("==== Llama 3.2 Generating Analysis ====")
+    print("==== Deepseek Generating Analysis ====")
     response_text = ""
 
     try:
-        for i, chunk in enumerate(generate("llama3.2:latest", prompt, stream=True)):
+        for i, chunk in enumerate(generate("deepseek-r1:1.5b", prompt, stream=True)):
             response_text += chunk.get("response", "")
             print(chunk.get("response", ""), end="", flush=True)
             if max_chunks and (i + 1) >= max_chunks:
