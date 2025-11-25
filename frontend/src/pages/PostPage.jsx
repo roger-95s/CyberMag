c2ec3e46b1bba62a3f6f94e92b0a681ddf7e4220
roger-95s <rogeresleitercc@gmail.com>
2025-11-25 07:41:21 -0800

    Added ReportCard at PostPage

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,18 +1,14 @@
 // import { useState, useEffect } from "react";
 import Header from "../components/Header";
-// import AllPosts from "../components/Postsreport";
-// import ReportCard from "../components/ReportCard.jsx";
-import ReportPage from "./ReportPage";
+import ReportCard from "../components/ReportCard.jsx";
+
 
 function PostPage() {
-
     return (
         <div className="min-h-screen bg-white dark:bg-[#0a0f1a] text-gray-900 dark:text-gray-100 transition-colors duration-500">
             <Header />
             <h1 className="text-4xl font-bold text-center my-8">All Posts</h1>
-            {/* <AllPosts /> */}
-            {/* <ReportCard /> */}
-            < ReportPage />
+            <ReportCard />
         </div>
     );
 }
